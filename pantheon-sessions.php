104adf4c91dd07987614e62cb1d8600f4694517d
Daniel Bachhuber <d@danielbachhuber.com>
2014-08-06 17:58:10 -0400

    Initial commit of scaffold

---
+++ pantheon-sessions.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Pantheon Sessions for WordPress
+Version: 0.1-alpha
+Description: Offload PHP sessions to your database for multi-server compatibility.
+Author: Pantheon
+Author URI: https://www.getpantheon.com/
+Plugin URI: https://www.getpantheon.com/
+Text Domain: pantheon-sessions
+Domain Path: /languages
+*/
+
