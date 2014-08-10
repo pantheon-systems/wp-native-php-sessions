841f15f21d707b9f2988359e663b1970f7beab01
Daniel Bachhuber <d@danielbachhuber.com>
2014-08-10 08:19:54 -0700

    Create the database to store sessions

--- tests/test-init-plugin.php
+++ tests/test-init-plugin.php
@@ -7,6 +7,21 @@ class Test_Init_Plugin extends WP_UnitTestCase {
 	}
 
 	public function test_database_created() {
+		global $wpdb, $table_prefix;
+
+		$table_name = "{$table_prefix}pantheon_sessions";
+		$this->assertEquals( $table_name, $wpdb->pantheon_sessions );
+
+		$column_data = $wpdb->get_results( "SHOW COLUMNS FROM {$table_name}" );
+		$columns = wp_list_pluck( $column_data, 'Field' );
+		$this->assertEquals( array(
+			'user_id',
+			'session_id',
+			'secure_session_id',
+			'hostname',
+			'timestamp',
+			'session',
+			), $columns );
 
 	}
 
