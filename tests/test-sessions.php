e33705e240b42646df813a042cfdd6de2fe9f395
Daniel Bachhuber <d@danielbachhuber.com>
2014-08-14 17:05:12 -0700

    Retool the testing layout

--- tests/test-sessions.php
+++ tests/test-sessions.php
@@ -2,38 +2,45 @@
 
 class Test_Sessions extends WP_UnitTestCase {
 
-	public function test_session_crud() {
+	protected $mock_session_id = 'SESSabc123';
 
-		/*
-		 * Create
-		 */
-		@session_start(); // Produces notice: session_start(): Cannot send session cookie - headers already sent by (output started at /tmp/wordpress-tests-lib/includes/bootstrap.php:53)
+	public function setUp() {
+		parent::setUp();
+		ob_start();
+		@session_start();
+	}
+
+	public function test_session_id() {
 		$session_id = session_id();
 		$this->assertNotEmpty( $session_id );
-
-		/*
-		 * Update / Read
-		 */
-		$_SESSION['foo'] = 'bar';
-		session_commit();
-		$session = \Pantheon_Sessions\Session::get_by_sid( $session_id );
-		$data = $session->get_data();
-		$this->assertEquals( 'foo|s:3:"bar";', $session->get_data() );
-
-		/*
-		 * Destroy
-		 */
-		@session_destroy(); // Produces notice: session_destroy(): Trying to destroy uninitialized session
-		$session->destroy();
-		$session = \Pantheon_Sessions\Session::get_by_sid( $session_id );
-		$this->assertFalse( $session );
-		$this->assertEmpty( $_SESSION );
-
 	}
 
 	public function test_session_name() {
 		$session_name = session_name();
 		$this->assertStringStartsWith( "SESS", $session_name );
+	}
+
+	public function test_session_write_read() {
+		$_SESSION['foo'] = 'bar';
+		session_commit();
+		$session = \Pantheon_Sessions\Session::get_by_sid( session_id() );
+		$data = $session->get_data();
+		$this->assertEquals( 'foo|s:3:"bar";', $session->get_data() );
+		return $session;
+	}
+
+	/**
+	 * @depends test_session_write_read
+	 */
+	public function test_session_destroy( $session ) {
+		$session->destroy();
+		$session = \Pantheon_Sessions\Session::get_by_sid( session_id() );
+		$this->assertFalse( $session );
+		$this->assertEmpty( $_SESSION );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
 	}
 
 }
