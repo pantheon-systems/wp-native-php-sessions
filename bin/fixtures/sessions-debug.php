3269699ac17f2e3846a25b1c6d356248fb4d0811
Daniel Bachhuber <daniel@handbuilt.co>
2016-07-11 15:03:41 -0700

    Use template_redirect for easier testing with a logged-in user
    This means we only need to register against one action, instead of two

--- bin/fixtures/sessions-debug.php
+++ bin/fixtures/sessions-debug.php
@@ -8,27 +8,39 @@
 # INSECURE. DO NOT USE WHERE SECURITY IS A CONCERN.
 #####################
 
-add_action( 'wp_ajax_nopriv_wpnps_get_session', function() {
+add_action( 'template_redirect', function() {
+	if ( 'wpnps_get_session' !== $_GET['action'] ) {
+		return;
+	}
 	session_start();
 	echo "(" . $_GET['key'] . ':' . $_SESSION[ $_GET['key'] ] . ")";
 	exit;
 });
 
-add_action( 'wp_ajax_nopriv_wpnps_set_session', function() {
+add_action( 'template_redirect', function() {
+	if ( 'wpnps_set_session' !== $_GET['action'] ) {
+		return;
+	}
 	session_start();
 	$_SESSION[ $_GET['key'] ] = $_GET['value'];
 	echo 'Session updated.';
 	exit;
 });
 
-add_action( 'wp_ajax_nopriv_wpnps_delete_session', function() {
+add_action( 'template_redirect', function() {
+	if ( 'wpnps_delete_session' !== $_GET['action'] ) {
+		return;
+	}
 	session_start();
 	unset( $_SESSION[ $_GET['key'] ] );
 	echo 'Session deleted.';
 	exit;
 });
 
-add_action( 'wp_ajax_nopriv_wpnps_check_table', function() {
+add_action( 'template_redirect', function() {
+	if ( 'wpnps_check_table' !== $_GET['action'] ) {
+		return;
+	}
 	global $wpdb;
 	$results = $wpdb->get_results( "SELECT user_id,data FROM {$wpdb->pantheon_sessions}" );
 	foreach( $results as $result ) {
@@ -37,7 +49,10 @@ add_action( 'wp_ajax_nopriv_wpnps_check_table', function() {
 	exit;
 });
 
-add_action( 'wp_ajax_nopriv_wpnps_plugin_loaded', function() {
+add_action( 'template_redirect', function() {
+	if ( 'wpnps_plugin_loaded' !== $_GET['action'] ) {
+		return;
+	}
 	session_start();
 	if ( class_exists( 'Pantheon_Sessions' ) && PANTHEON_SESSIONS_ENABLED ) {
 		echo 'Plugin is loaded.';
