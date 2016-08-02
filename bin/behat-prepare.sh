1a0171dade78da836be670a5ab78c64d4bf267ba
Steve Persch <steve.persch@pantheon.io>
2016-08-02 10:16:20 -0500

    Restoring most of bin/behat-prepare.sh

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -6,6 +6,10 @@
 # such that it can be run a second time if a step fails.
 ###
 
+if [ -z "$TERMINUS_TOKEN" ]; then
+	echo "TERMINUS_TOKEN environment variables missing; assuming unauthenticated build"
+	exit 0
+fi
 
 set -ex
 
@@ -17,9 +21,7 @@ fi
 ###
 # Create a new environment for this particular test run.
 ###
-#terminus site create-env --to-env=$TERMINUS_ENV --from-env=dev
-
-
+terminus site create-env --to-env=$TERMINUS_ENV --from-env=dev
 yes | terminus site wipe
 
 ###
@@ -31,10 +33,45 @@ PREPARE_DIR="/tmp/$TERMINUS_ENV-$TERMINUS_SITE"
 BASH_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ###
+# Switch to git mode for pushing the files up
+###
+terminus site set-connection-mode --mode=git
+rm -rf $PREPARE_DIR
+git clone -b $TERMINUS_ENV $PANTHEON_GIT_URL $PREPARE_DIR
+
+###
+# Add the copy of this plugin itself to the environment
+###
+rm -rf $PREPARE_DIR/wp-content/plugins/wp-native-php-sessions
+cd $BASH_DIR/..
+rsync -av --exclude='vendor/' --exclude='node_modules/' --exclude='tests/' ./* $PREPARE_DIR/wp-content/plugins/wp-native-php-sessions
+rm -rf $PREPARE_DIR/wp-content/plugins/wp-native-php-sessions/.git
+
+
+###
+# Add the debugging plugin to the environment
+###
+rm -rf $PREPARE_DIR/wp-content/mu-plugins/sessions-debug.php
+cp $BASH_DIR/fixtures/sessions-debug.php $PREPARE_DIR/wp-content/mu-plugins/sessions-debug.php
+
+###
+# Push files to the environment
+###
+cd $PREPARE_DIR
+git add wp-content
+git config user.email "wp-native-php-sessions@getpantheon.com"
+git config user.name "Pantheon"
+git commit -m "Include WP Native PHP Sessions and its configuration files"
+git push
+
+# Sometimes Pantheon takes a little time to refresh the filesystem
+sleep 10
+
+###
 # Set up WordPress, theme, and plugins for the test run
 ###
 # Silence output so as not to show the password.
 {
-terminus wp "core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=wp-native-php-sessions@getpantheon.com --admin_password=$WORDPRESS_ADMIN_PASSWORD"
- } &> /dev/null
+  terminus wp "core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=wp-native-php-sessions@getpantheon.com --admin_password=$WORDPRESS_ADMIN_PASSWORD"
+} &> /dev/null
 terminus wp "plugin activate wp-native-php-sessions"
