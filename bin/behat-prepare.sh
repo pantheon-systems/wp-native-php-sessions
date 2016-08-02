7f07cf177b0d02b58d4c7674aebee89f8f1a5091
Steve Persch <steve.persch@pantheon.io>
2016-08-02 09:46:22 -0500

    output debugging

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -6,10 +6,6 @@
 # such that it can be run a second time if a step fails.
 ###
 
-if [ -z "$TERMINUS_TOKEN" ]; then
-	echo "TERMINUS_TOKEN environment variables missing; assuming unauthenticated build"
-	exit 0
-fi
 
 set -ex
 
@@ -22,12 +18,23 @@ fi
 # Create a new environment for this particular test run.
 ###
 #terminus site create-env --to-env=$TERMINUS_ENV --from-env=dev
-yes | terminus site wipe
 
 
+#yes | terminus site wipe
+
+###
+# Get all necessary environment details.
+###
+PANTHEON_GIT_URL=$(terminus site connection-info --field=git_url)
+PANTHEON_SITE_URL="$TERMINUS_ENV-$TERMINUS_SITE.pantheonsite.io"
+PREPARE_DIR="/tmp/$TERMINUS_ENV-$TERMINUS_SITE"
+BASH_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ###
 # Set up WordPress, theme, and plugins for the test run
 ###
-terminus wp "core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=wp-redis@getpantheon.com --admin_password=$WORDPRESS_ADMIN_PASSWORD" &> /dev/null
+# Silence output so as not to show the password.
+{
+terminus wp "core install --title=$TERMINUS_ENV-$TERMINUS_SITE --url=$PANTHEON_SITE_URL --admin_user=$WORDPRESS_ADMIN_USERNAME --admin_email=wp-native-php-sessions@getpantheon.com --admin_password=$WORDPRESS_ADMIN_PASSWORD"
+ } &> /dev/null
 terminus wp "plugin activate wp-native-php-sessions"
