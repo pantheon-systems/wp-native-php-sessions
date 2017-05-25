7b3aa97cb300826441f702c1a3608b1ce9967de6
Daniel Bachhuber <daniel@handbuilt.co>
2017-05-25 13:03:52 -0700

    Update README for 0.6.1

--- pantheon-sessions.php
+++ pantheon-sessions.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Native PHP Sessions for WordPress
-Version: 0.6.0
+Version: 0.6.1
 Description: Offload PHP's native sessions to your database for multi-server compatibility.
 Author: Pantheon
 Author URI: https://www.pantheon.io/
