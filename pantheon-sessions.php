570e71d67cbf8a7c9de5d0016dd10f19bb83c69a
John Spellman <john.spellman@pantheon.io>
2023-01-25 10:35:55 -0700

    Release 1.3.3

--- pantheon-sessions.php
+++ pantheon-sessions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Native PHP Sessions for WordPress
- * Version: 1.3.1
+ * Version: 1.3.3
  * Description: Offload PHP's native sessions to your database for multi-server compatibility.
  * Author: Pantheon
  * Author URI: https://www.pantheon.io/
@@ -13,7 +13,7 @@
 
 use Pantheon_Sessions\Session;
 
-define( 'PANTHEON_SESSIONS_VERSION', '1.3.2' );
+define( 'PANTHEON_SESSIONS_VERSION', '1.3.3' );
 
 /**
  * Main controller class for the plugin.
