pantheon-systems/wp-native-php-sessions

commit 072f7cb9fbc7fa7140b867a41487c7d447756954

Enhance informational Error message for the case of headers already sent (#249)
* Enhance informational Error message for the case of headers already sent

This code update enhances the error message displayed by the plugin when it is unable to perform its intended functionality due to headers already being sent. The original error message was "_session_set_save_handler(): Session save handler cannot be changed after headers have already been sent_" has been transformed into a more user-friendly and informative message. The updated error message helps users understand that the plugin was unable to work properly and provides insight into the cause, such as PHP throwing errors, the case that happened for me. By improving the error message, users can better troubleshoot and resolve the issue instead of thinking that wp-native-php-sessions plugin falied.

* Add textdomain & fix spacing

* fix spacing

---------

Co-authored-by: Chris Reynolds <chris@jazzsequence.com>

pantheon-sessions.php
@@ -164,7 +164,21 @@ class Pantheon_Sessions {
 		require_once __DIR__ . '/inc/class-session-handler.php';
 		$session_handler = new Pantheon_Sessions\Session_Handler();
 		if ( PHP_SESSION_ACTIVE !== session_status() ) {
-			session_set_save_handler( $session_handler, false );
+			// Check if headers have already been sent
+			if ( headers_sent( $file, $line ) ) {
+				// Output a friendly error message if headers are already sent
+				trigger_error(
+					sprintf(
+						/* translators: %1s: File path, %2d: Line number */
+						__( "Oops! The wp-native-php-sessions plugin couldn't start the session because output has already been sent. This might be caused by PHP throwing errors. Please check the code in %1s on line %2d.", 'wp-native-php-sessions' ),
+						$file,
+						$line
+					),
+					E_USER_WARNING
+				);
+			} else {
+				session_set_save_handler( $session_handler, false );
+			}
 		}
 		// Close the session before $wpdb destructs itself.
 		add_action( 'shutdown', 'session_write_close', 999, 0 );

commit ad14a22c73089c9e33d30416235eb37f1ba6573d

CONTRIBUTING.md updates (#252)
* update contrib.md and version number

* use correct pr number

* fix lint error introduced in #249

pantheon-sessions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Native PHP Sessions for WordPress
- * Version: 1.3.5
+ * Version: 1.3.6-dev
  * Description: Offload PHP's native sessions to your database for multi-server compatibility.
  * Author: Pantheon
  * Author URI: https://www.pantheon.io/
@@ -13,7 +13,7 @@
 
 use Pantheon_Sessions\Session;
 
-define( 'PANTHEON_SESSIONS_VERSION', '1.3.5' );
+define( 'PANTHEON_SESSIONS_VERSION', '1.3.6-dev' );
 
 /**
  * Main controller class for the plugin.
@@ -164,9 +164,9 @@ class Pantheon_Sessions {
 		require_once __DIR__ . '/inc/class-session-handler.php';
 		$session_handler = new Pantheon_Sessions\Session_Handler();
 		if ( PHP_SESSION_ACTIVE !== session_status() ) {
-			// Check if headers have already been sent
+			// Check if headers have already been sent.
 			if ( headers_sent( $file, $line ) ) {
-				// Output a friendly error message if headers are already sent
+				// Output a friendly error message if headers are already sent.
 				trigger_error(
 					sprintf(
 						/* translators: %1s: File path, %2d: Line number */