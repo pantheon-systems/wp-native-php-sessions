94076ebf43e474f5d26e8e00fc5759729e0b4b72
Ryan Wagner <ryan.wagner@pantheon.io>
2023-09-21 17:57:10 -0400

    BUGS-6563: Add tests back somewhere else.

--- tests/phpunit/test-sessions.php
+++ tests/phpunit/test-sessions.php
@@ -173,6 +173,49 @@ class Test_Sessions extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Ensure that the primary key addition command works.
+	 */
+	public function test_primary_key_addition() {
+		global $wpdb, $table_prefix;
+
+		$table_name = "{$table_prefix}pantheon_sessions";
+		$pantheon_session = new Pantheon_Sessions();
+
+		$query = "ALTER TABLE {$table_name} DROP COLUMN id";
+		$wpdb->query( $query );
+
+		$column_data = $wpdb->get_results( "SHOW COLUMNS FROM {$table_name}" );
+		$columns     = wp_list_pluck( $column_data, 'Field' );
+		$this->assertEquals(
+			[
+				'user_id',
+				'session_id',
+				'secure_session_id',
+				'ip_address',
+				'datetime',
+				'data',
+			],
+			$columns
+		);
+
+		$pantheon_session->add_index( '', '' );
+		$column_data = $wpdb->get_results( "SHOW COLUMNS FROM {$table_name}" );
+		$columns     = wp_list_pluck( $column_data, 'Field' );
+		$this->assertEquals(
+			[
+				'id',
+				'user_id',
+				'session_id',
+				'secure_session_id',
+				'ip_address',
+				'datetime',
+				'data',
+			],
+			$columns
+		);
+	}
+
+	/**
 	 * Runs at the end of every test.
 	 */
 	public function tearDown(): void {
