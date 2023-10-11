817974cdbbeb24daf11c903ab4ad049055e0b06f
Ryan Wagner <ryan.wagner@pantheon.io>
2023-10-11 17:29:25 -0400

    [CMSP-678]: Shellcheck

--- bin/full-teardown-wp-tests.sh
+++ bin/full-teardown-wp-tests.sh
@@ -29,9 +29,9 @@ drop_db() {
 	if [ -n "$DB_HOSTNAME" ] ; then
 		if [ $(echo "$DB_SOCK_OR_PORT" | grep -e '^[0-9]\{1,\}$') ]; then
 			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-		elif ! [ -z "$DB_SOCK_OR_PORT" ] ; then
+		elif [ -n "$DB_SOCK_OR_PORT" ] ; then
 			EXTRA=" --socket=$DB_SOCK_OR_PORT"
-		elif ! [ -z "$DB_HOSTNAME" ] ; then
+		elif [ -n "$DB_HOSTNAME" ] ; then
 			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
 		fi
 	fi
