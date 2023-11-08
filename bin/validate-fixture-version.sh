07099c8813cb53b7e9c3b54741e4863121f04608
Chris Reynolds <chris@jazzsequence.com>
2023-11-08 15:47:23 -0700

    use &1

--- bin/validate-fixture-version.sh
+++ bin/validate-fixture-version.sh
@@ -31,7 +31,7 @@ main(){
     TESTED_UP_TO=$(grep -i "Tested up to:" "${README_FILE_PATH}" | tr -d '\r\n' | awk -F ': ' '{ print $2 }')
     echo "Tested Up To: ${TESTED_UP_TO}"
     local FIXTURE_VERSION
-    FIXTURE_VERSION=$(terminus wp "${TERMINUS_SITE}.dev" -- core version 2>/dev/null || true)
+    FIXTURE_VERSION=$(terminus wp "${TERMINUS_SITE}.dev" -- core version 2>&1)
     echo "Fixture Version: ${FIXTURE_VERSION}"
 
     if ! php -r "exit(version_compare('${TESTED_UP_TO}', '${FIXTURE_VERSION}'));"; then
