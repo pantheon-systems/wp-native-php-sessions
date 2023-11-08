1d05ab72245738e81418bd8ef2e2d883915db495
Chris Reynolds <chris@jazzsequence.com>
2023-11-08 15:10:15 -0700

    remove non-existant --from-env

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -22,12 +22,12 @@ if [ -v PHP_VERSION ] && [ "$PHP_VERSION" == "8.3" ]; then
 	###
 	# Create a new environment for this particular test run on PHP 8.3.
 	###
-	terminus multidev:create "$TERMINUS_SITE".83 --from-env="83-$TERMINUS_ENV" --yes
+	terminus multidev:create "$TERMINUS_SITE".83 "83-$TERMINUS_ENV" --yes
 else
 	###
 	# Create a new environment for this particular test run on PHP 7.4.
 	###
-	terminus multidev:create "$TERMINUS_SITE".dev --from-env="$TERMINUS_ENV" --yes
+	terminus multidev:create "$TERMINUS_SITE".dev "$TERMINUS_ENV" --yes
 fi
 terminus env:wipe "$SITE_ENV" --yes
 
