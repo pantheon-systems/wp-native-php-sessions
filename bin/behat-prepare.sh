96c1956e4740daf3fce9d32ce949387d120e5058
Andrii KOSTENETSKYI <andrii.kostenetsky@pantheon.io>
2025-10-10 11:53:39 +0300

    feat: SRE-1122 - Migration from CircleCI to GH Actions

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -6,6 +6,21 @@
 # such that it can be run a second time if a step fails.
 ###
 
+if [ -z "$TERMINUS_TOKEN" ]; then
+	echo "TERMINUS_TOKEN environment variables missing; assuming unauthenticated build"
+	exit 0
+fi
+
+# Exit immediately, but don't expose $TERMINUS_TOKEN
+set -e
+set +x
+
+git clone --branch master https://github.com/pantheon-systems/terminus.git ~/terminus
+cd ~/terminus && composer install
+terminus auth:login --machine-token=$TERMINUS_TOKEN
+terminus self:plugin:install terminus-build-tools-plugin
+
+
 if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
 	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
 	exit 1
